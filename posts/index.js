c49d438da7ab8b1d6152a43235d85763eabf6852
Jean Bauer <jeanbauerc@gmail.com>
2020-05-06 14:17:27 +0200

    Add in memory posts service

---
+++ posts/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const { randomBytes } = require('crypto')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+// In memory posts
+const posts = {}
+
+app.get('/posts', (req, res) => {
+  res.send(posts)
+})
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex')
+
+  const { title } = req.body
+  posts[id] = { id, title }
+
+  res.status(201).send(posts[id])
+})
+
+app.listen(4000, () => console.log('Listening on 4000'))
