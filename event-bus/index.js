0eef8e1ce981350ea943a3b4a2cda998dfa458af
Jean Bauer <jeanbauerc@gmail.com>
2020-05-08 11:08:38 +0200

    Add sync events feature to event bus :sparkles:

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser.json())
 
+const events = []
+
 app.post('/events', (req, res) => {
   const event = req.body
+
+  events.push(event)
 
   axios.post('http://localhost:4000/events', event) // posts
   axios.post('http://localhost:4001/events', event) // comments
@@ -14,6 +18,10 @@ app.post('/events', (req, res) => {
   axios.post('http://localhost:4003/events', event) // moderation
 
   res.send({ status: 'OK' })
+})
+
+app.get('/events', (req, res) => {
+  res.send(events)
 })
 
 app.listen(4005, () => console.log('listening on 4005'))
